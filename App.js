39544ebef6860cf3d532ddc2967b4e6e1781e284
Jyothesh Karnam <96298283+jyotheshkar@users.noreply.github.com>
2022-04-21 13:53:29 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Setup from './tutorial/1-useState/setup/5-useState-counter';
+//import { useState } from 'react';
+
+function App() {
+  return (
+    <div className='container'>
+     
+      < Setup /> 
+      <useState />
+      
+    </div>
+  )
+}
+
+export default App
